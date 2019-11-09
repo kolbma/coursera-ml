daed41608278add9eb16ca68528c5c82dcf77f9c
Markus Kolb <github@tower-net.de>
2019-11-09 22:05:08 +0100

    fix: help logic

--- ex1/computeCost_main.cpp
+++ ex1/computeCost_main.cpp
@@ -9,30 +9,34 @@ using namespace arma;
 
 int main(int argc, char **argv)
 {
+    if (argc >= 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0))
+    {
+        cout << argv[0] << ": usage \"X\" \"y\" \"theta\"" << endl
+             << "\t example matrix \"[1,2;3,4;5,6]\"" << endl
+             << "\t example vector \"[7;8;9]\"" << endl;
+        return 0;
+    }
     if (argc != 4)
     {
-        if (argc == 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0))
-        {
-            cout << argv[0] << ": usage \"X\" \"y\" \"theta\"" << endl
-                 << "\t example matrix \"[1,2;3,4;5,6]\"" << endl
-                 << "\t example vector \"[7;8;9]\"" << endl;
-            return 0;
-        }
-        else
-        {
-            cerr << argv[0] << ": missing parameters (see --help)" << endl;
-            return 1;
-        }
+        cerr << argv[0] << ": missing parameters (see --help)" << endl;
+        return 1;
     }
 
     fmat X;
-    parse_mat(&X, argv[1]);
+    fvec y, theta;
 
-    fvec y;
-    parse_mat(&y, argv[2]);
-
-    fvec theta;
-    parse_mat(&theta, argv[3]);
+    try
+    {
+        parse_mat(&X, argv[1]);
+        parse_mat(&y, argv[2]);
+        parse_mat(&theta, argv[3]);
+    }
+    catch (const exception &e)
+    {
+        cerr << argv[0] << ": parameter not parseable (see --help)" << endl
+             << e.what() << endl;
+        return 1;
+    }
 
     cout << "J = " << computeCost(X, y, theta) << endl;
 
